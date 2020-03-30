51243021c02d835b0276fe7d46d929f52d7a352b
eunyuni <eunyuuni@gmail.com>
2020-03-30 22:02:35 +0900

    Schedule update

---
+++ DabangClone/DabangClone/MoreView/View/Cells/PriceBottomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PriceBottomTableViewCell.swift
+//  DabangClone
+//
+//  Created by 은영김 on 2020/03/30.
+//  Copyright © 2020 pandaman. All rights reserved.
+//
+
+import UIKit
+
+class PriceBottomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DabangClone/DabangClone/MoreView/View/SaveView.swift
@@ -0,0 +1,21 @@
+//
+//  SaveView.swift
+//  DabangClone
+//
+//  Created by 은영김 on 2020/03/30.
+//  Copyright © 2020 pandaman. All rights reserved.
+//
+
+import UIKit
+
+class SaveView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
