5733c386270c2fe503e25bd3beae7d5c4e13ae0a
eunyuni <eunyuuni@gmail.com>
2020-03-31 21:40:51 +0900

    Schedule update

---
+++ DabangClone/DabangClone/MoreView/View/Cells/RSBodyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RSBodyTableViewCell.swift
+//  DabangClone
+//
+//  Created by 은영김 on 2020/03/31.
+//  Copyright © 2020 pandaman. All rights reserved.
+//
+
+import UIKit
+
+class RSBodyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DabangClone/DabangClone/MoreView/View/Cells/RSTopTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RSTopTableViewCell.swift
+//  DabangClone
+//
+//  Created by 은영김 on 2020/03/31.
+//  Copyright © 2020 pandaman. All rights reserved.
+//
+
+import UIKit
+
+class RSTopTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
