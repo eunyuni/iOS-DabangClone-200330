3bbc90146b33dc55db901d24c4217f43358eca13
eunyuni <eunyuuni@gmail.com>
2020-04-03 18:18:00 +0900

    Fix: Schedule_April_1

---
+++ DabangClone/DabangClone/MapView/Controller/Padding.swift
@@ -0,0 +1,9 @@
+//
+//  Padding.swift
+//  DabangClone
+//
+//  Created by 은영김 on 2020/04/03.
+//  Copyright © 2020 pandaman. All rights reserved.
+//
+
+import Foundation
---
+++ DabangClone/DabangClone/MapView/FilterRoomTypeCell.swift
@@ -0,0 +1,24 @@
+//
+//  FilterRoomTypeCell.swift
+//  DabangClone
+//
+//  Created by 은영김 on 2020/04/03.
+//  Copyright © 2020 pandaman. All rights reserved.
+//
+
+import UIKit
+
+class FilterRoomTypeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
