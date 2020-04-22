f3640389bc202876417c972c13610dde9add74f6
dukdamn <dukdamn0927@gmail.com>
2020-04-22 21:16:04 +0900

    Fix: GoogleMap / 정덕

--- DabangClone/DabangClone/MapView/Controller/MapViewController.swift
+++ DabangClone/DabangClone/MapView/Controller/MapViewController.swift
@@ -15,7 +15,7 @@ public let DEFAULT_CAMERA_POSITION = GMSCameraPosition(latitude: 37.5666102, lon
 class MapViewController: UIViewController{
   // MARK: - Property
   
-  private var mapTest = GMSMapView.map(withFrame: .zero, camera: DEFAULT_CAMERA_POSITION).then {
+  private var mapTest = GMSMapView().then {
     $0.showsLargeContentViewer = true
     $0.settings.myLocationButton = true
     $0.settings.indoorPicker = true
@@ -31,14 +31,12 @@ class MapViewController: UIViewController{
     locationManager.desiredAccuracy = kCLLocationAccuracyBest
     locationManager.startUpdatingLocation()
     let coor = locationManager.location?.coordinate
-    let latitude = coor?.latitude
-    let longtitude = coor?.longitude
-    print(latitude,longtitude)
-    GMSCameraPosition.camera(withLatitude: -33,
-    longitude: 151,
-    zoom: 6,
-    bearing: 270,
-    viewingAngle: 45)
+    let latitude = coor?.latitude ?? 37.5666102
+    let longtitude = coor?.longitude ?? 126.9783881
+        print(latitude,longtitude)
+    mapTest.camera = GMSCameraPosition.camera(withLatitude: 37.5666102,
+                                              longitude: 126.9783881,
+                                              zoom: 18)
     setupUI()
   }
   // MARK: - Action
@@ -46,18 +44,12 @@ class MapViewController: UIViewController{
   private func setupUI() {
     mapTest.delegate = self
     self.view.addSubviews([mapTest])
-    // Creates a marker in the center of the map.
-    let marker = GMSMarker()
-    marker.position = CLLocationCoordinate2D(latitude: 37.5666102, longitude: 126.9783881)
-    marker.title = "Sydney"
-    marker.snippet = "Australia"
-    marker.map = mapTest
     setupConstraint()
   }
   // MARK: - setupConstraint
   private func setupConstraint() {
     mapTest.snp.makeConstraints {
-      $0.edges.equalToSuperview().inset(40)
+      $0.edges.equalToSuperview()
     }
   }
 }
