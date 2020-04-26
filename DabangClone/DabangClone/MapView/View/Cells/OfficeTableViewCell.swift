f9da3597acba133678974b94e60e7d9a96793768
dukdamn <dukdamn0927@gmail.com>
2020-04-26 17:37:50 +0900

    Add: OfficeTableCell/정덕

---
+++ DabangClone/DabangClone/MapView/View/Cells/OfficeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OfficeTableViewCell.swift
+//  DabangClone
+//
+//  Created by 황정덕 on 2020/04/24.
+//  Copyright © 2020 pandaman. All rights reserved.
+//
+
+import UIKit
+
+class OfficeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
